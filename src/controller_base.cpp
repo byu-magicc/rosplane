88da06cd4c20b5a853d95c18dbe39ab575bd007d
Gary Ellingson <gellings13@gmail.com>
2016-02-22 14:39:48 -0700

    fixed a couple small bugs

--- src/controller_base.cpp
+++ src/controller_base.cpp
@@ -14,19 +14,19 @@ controller_base::controller_base():
     nh_private_.param<float>("TRIM_E", _params.trim_e, 0.0f);
     nh_private_.param<float>("TRIM_A", _params.trim_a, 0.0f);
     nh_private_.param<float>("TRIM_R", _params.trim_r, 0.0f);
-    nh_private_.param<float>("TRIM_T", _params.trim_t, 0.0f);
+    nh_private_.param<float>("TRIM_T", _params.trim_t, 0.6f);
     nh_private_.param<float>("PWM_RAD_E", _params.pwm_rad_e, 1.0f);
     nh_private_.param<float>("PWM_RAD_A", _params.pwm_rad_a, 1.0f);
     nh_private_.param<float>("PWM_RAD_R", _params.pwm_rad_r, 1.0f);
     nh_private_.param<float>("ALT_TOZ", _params.alt_toz, 20.0f);
     nh_private_.param<float>("ALT_HZ", _params.alt_hz, 10.0f);
     nh_private_.param<float>("TAU", _params.tau, 5.0f);
-    nh_private_.param<float>("COURSE_KP", _params.c_kp, 0.4657f);
+    nh_private_.param<float>("COURSE_KP", _params.c_kp, 0.5*0.4657f);
     nh_private_.param<float>("COURSE_KD", _params.c_kd, 0.0f);
-    nh_private_.param<float>("COURSE_KI", _params.c_ki, 0.0596f);
+    nh_private_.param<float>("COURSE_KI", _params.c_ki, 0.0*0.0596f);
     nh_private_.param<float>("ROLL_KP", _params.r_kp, 1.2855f);
     nh_private_.param<float>("ROLL_KD", _params.r_kd, 0.0934f);
-    nh_private_.param<float>("ROLL_KI", _params.r_ki, 0.10f);
+    nh_private_.param<float>("ROLL_KI", _params.r_ki, 0.0f);//0.10f);
     nh_private_.param<float>("PITCH_KP", _params.p_kp, 1.0f);
     nh_private_.param<float>("PITCH_KD", _params.p_kd, -0.1168f);
     nh_private_.param<float>("PITCH_KI", _params.p_ki, 0.0f);
@@ -72,7 +72,7 @@ void controller_base::convert_to_pwm(controller_base::output_s &output)
 void controller_base::actuator_controls_publish(const ros::TimerEvent&)
 {
     struct input_s input;
-    input.h = _vehicle_state.position[2];
+    input.h = -_vehicle_state.position[2];
     input.va = _vehicle_state.Va;
     input.phi = _vehicle_state.phi;
     input.theta = _vehicle_state.theta;
@@ -83,8 +83,7 @@ void controller_base::actuator_controls_publish(const ros::TimerEvent&)
     input.Va_c = _controller_commands.Va_c;
     input.h_c = _controller_commands.h_c;
     input.chi_c = _controller_commands.chi_c;
-//        hrt_abstime curr_time = hrt_absolute_time();
-    input.Ts = 0.01f;//(prev_time_ != 0) ? (curr_time - prev_time_) * 0.000001f : 0.0f;// 0.01f;
+    input.Ts = 0.01f;
 
     struct output_s output;
 
