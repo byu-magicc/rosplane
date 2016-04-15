6d7f6f5bd58f80eac04da268778428deba8e8b25
Dallin Briggs <dallinbriggs@gmail.com>
2016-04-15 13:33:55 -0600

    Created empty class for path follower

---
+++ include/path_follower.h
@@ -0,0 +1,10 @@
+#ifndef PATH_FOLLOWER_H
+#define PATH_FOLLOWER_H
+
+class path_follower
+{
+public:
+  path_follower();
+};
+
+#endif // PATH_FOLLOWER_H
---
+++ src/path_follower.cpp
@@ -0,0 +1,5 @@
+#include "path_follower.h"
+
+path_follower::path_follower()
+{
+}
