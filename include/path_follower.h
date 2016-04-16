b7c4e64e424edb6b450ec6e97ebf2d7eeab5defc
Dallin Briggs <dallinbriggs@gmail.com>
2016-04-16 15:35:33 -0600

    Added gary's stuff to path follower class

--- include/path_follower.h
+++ include/path_follower.h
@@ -1,49 +1,58 @@
 #ifndef PATH_FOLLOWER_H
 #define PATH_FOLLOWER_H
 
-#include <ros/ros.h>
-#include <fcu_common/FW_State.h>
-#include <fcu_common/FW_Controller_Commands.h>
-#include <fcu_common/Command.h>
+#include "path_follower_base.h"
 
-#include <dynamic_reconfigure/server.h>
-#include <ros_plane/ControllerConfig.h>
+//#include <ros/ros.h>
+//#include <fcu_common/FW_State.h>
+//#include <fcu_common/FW_Controller_Commands.h>
+//#include <fcu_common/Command.h>
+
+//#include <dynamic_reconfigure/server.h>
+//#include <ros_plane/ControllerConfig.h>
 
 namespace rosplane {
 
-
-class path_follower
-{
-public:
-  path_follower();
-  float spin();
-  virtual int getstate() = 0;
-
-protected:
-
-  struct inputs{
-    int flag;           // Straight path or orbit
-    float Va_d;          // desired airspeed
-    float r_path;
-    float q_path;
-    float c_orbit;
-    float rho_orbit;
-    float lam_orbit;
-    float pn;
-    float pe;
-    float chi;              /** course angle */
+  class path_follower : public path_follower_base
+  {
+  public:
+      path_follower();
+  private:
+      virtual void follow(const struct params_s &params, const struct input_s &input, struct output_s &output);
   };
 
-  struct outputs{
-    float Va_c;         // commanded airspeed
-    float h_c;          // commanded altitude
-    float chi_c;        // commanded course
-    float phi_ff;       // phi feed forward term for orbit
-  };
+//class path_follower
+//{
+//public:
+//  path_follower();
+//  float spin();
+//  virtual int getstate() = 0;
 
-private:
+//protected:
 
-};
+//  struct inputs{
+//    int flag;           // Straight path or orbit
+//    float Va_d;          // desired airspeed
+//    float r_path;
+//    float q_path;
+//    float c_orbit;
+//    float rho_orbit;
+//    float lam_orbit;
+//    float pn;
+//    float pe;
+//    float chi;              /** course angle */
+//  };
+
+//  struct outputs{
+//    float Va_c;         // commanded airspeed
+//    float h_c;          // commanded altitude
+//    float chi_c;        // commanded course
+//    float phi_ff;       // phi feed forward term for orbit
+//  };
+
+//private:
+
+//};
 
 } //end namespace
 #endif // PATH_FOLLOWER_H
--- src/path_follower.cpp
+++ src/path_follower.cpp
@@ -6,6 +6,46 @@ path_follower::path_follower()
 {
 }
 
+void path_follower::follow(const params_s &params, const input_s &input, output_s &output)
+{
+    if(input.flag) // follow straight line path specified by r and q
+    {
+        // compute wrapped version of the path angle
+        float chi_q = atan2f(input.q_path[1],input.q_path[0]);
+        while (chi_q - input.chi < -M_PI_F)
+            chi_q += 2*M_PI_F;
+        while (chi_q - input.chi > M_PI_F)
+            chi_q -= 2*M_PI_F;
+
+        float path_error = -sinf(chi_q)*(input.pn - input.r_path[0]) + cosf(chi_q)*(input.pe - input.r_path[1]);
+        // heading command
+        output.chi_c = chi_q - params.chi_infty*2/M_PI_F*atanf(params.k_path*path_error);
+
+        // desired altitude
+        float h_d = -input.r_path[2]-sqrtf(powf((input.r_path[0] - input.pn),2) + powf((input.r_path[1] - input.pe),2))*(input.q_path[2])/sqrtf(powf(input.q_path[0],2) + powf(input.q_path[1],2));
+        // commanded altitude is desired altitude
+        output.h_c = h_d;
+    }
+    else
+    {
+        float d = sqrtf(powf((input.pn - input.c_orbit[0]),2) + powf((input.pe - input.c_orbit[1]),2)); // distance from orbit center
+        // compute wrapped version of angular position on orbit
+        float varphi = atan2f(input.pe - input.c_orbit[1], input.pn - input.c_orbit[0]);
+        while (varphi - input.chi < -M_PI_F)
+            varphi += 2*M_PI_F;
+        while (varphi - input.chi > M_PI_F)
+            varphi -= 2*M_PI_F;
+        //compute orbit error
+        float orbit_error = d - input.rho_orbit;
+        output.chi_c = varphi + input.lam_orbit*(M_PI_F/2 + atanf(params.k_orbit*orbit_error));
+
+        // commanded altitude is the height of the orbit
+        float h_d = -input.c_orbit[2];
+        output.h_c = h_d;
+    }
+    output.Va_c = input.Va_d;
+}
+
 } //end namespace
 
 int main(int argc, char** argv) {
