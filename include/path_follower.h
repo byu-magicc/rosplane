feb643d3cc3bbe098f666543753d6bd1126b035c
Dallin Briggs <dallinbriggs@gmail.com>
2016-04-15 19:17:29 -0600

    Builds with blank path follower class

--- include/path_follower.h
+++ include/path_follower.h
@@ -1,10 +1,22 @@
 #ifndef PATH_FOLLOWER_H
 #define PATH_FOLLOWER_H
 
+#include <ros/ros.h>
+#include <fcu_common/FW_State.h>
+#include <fcu_common/FW_Controller_Commands.h>
+#include <fcu_common/Command.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <ros_plane/ControllerConfig.h>
+
+namespace rosplane {
+
+
 class path_follower
 {
 public:
   path_follower();
 };
 
+} //end namespace
 #endif // PATH_FOLLOWER_H
--- src/path_follower.cpp
+++ src/path_follower.cpp
@@ -1,5 +1,18 @@
 #include "path_follower.h"
 
+namespace rosplane {
+
 path_follower::path_follower()
 {
+}
+
+} //end namespace
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "ros_plane_follower");
+//  rosplane::controller_base* cont = new rosplane::controller_example();
+
+  ros::spin();
+
+  return 0;
 }
