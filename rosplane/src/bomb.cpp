aa861d69db1e64738c57b828614ad5877f39109e
Rob Neu <robpneu@gmail.com>
2018-06-09 18:15:24 -0600

    Write to the GPIO pins

--- rosplane/src/bomb.cpp
+++ rosplane/src/bomb.cpp
@@ -1,4 +1,5 @@
 #include <bomb.h>
+#include <wiringPi.h>
 
 namespace rosplane
 {
@@ -140,8 +141,7 @@ NED_t Bomb::calculateDropPoint(NED_t Vg3, double chi, double Va, double target_h
 void Bomb::dropNow()
 {
   // signal the okay to drop the bomb. GPIO CODE HERE
-
-
+  digitalWrite(0,LOW);
 
   ROS_WARN("DROPPING THE BOMB");
   // Do some post calculations
@@ -176,7 +176,7 @@ void Bomb::dropNow()
 void Bomb::armBomb()
 {
   // GPIO CODE HERE
-
+  digitalWrite(0,HIGH);
 
   bomb_armed_ = true;
 }
